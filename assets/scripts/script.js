8f85924aa3dd385b3a7d290ec5ef3ed3c205a375
Danny Calise <dannyjcalise@gmail.com>
2019-09-16 13:42:22 -0400

    added form for new button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,86 +1,53 @@
-// ### Instructions
-
-// 1. Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
 var topics = ["Post Malone", "Childish Gambino", "Frank Ocean", "Taylor Swift", "Lady Gaga", "Bradley Cooper", "Lana Del Rey", "Clairo", "Chance the Rapper", "Eminem", "Elton John", "Billy Joel", "Kanye West", "Lil Peep", "Travis Scott", "Beach Boys"];
-//    * We chose animals for our theme, but you can make a list to your own liking.
-
-// 2. Your app should take the topics in this array and create buttons in your HTML.
-//    * Try using a loop that appends a button for each string in the array.
 
 for (let i = 0; i < topics.length; i++) {
     $("#buttons").append(`<button class="btn btn-danger artist-button">${topics[i]}</button>`);    
 }
+$('#add').on('click', function() {
+    event.preventDefault();
+    var newTopic = $('#add-artist').val().trim();
+    topics.push(newTopic);
+    $("#buttons").append(`<button class="btn btn-danger artist-button">${newTopic}</button>`);
 
+    // console.log($('#add-artist').val().trim());
+});
 
-// 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-$(".artist-button").on("click", function() {
+function addGifs() {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aORSyh3OYX52nRTFnKFPMTC6ijlqSOee&q=&limit=25&offset=0&rating=R&lang=en";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
+   var artist = this.innerHTML;
+   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aORSyh3OYX52nRTFnKFPMTC6ijlqSOee&q=" + artist + "&limit=10&offset=0&rating=R&lang=en";
+    console.log(artist);
+    $.get(queryURL).then(function(response) {
         response.data.forEach((gif) => {
-            $("#giphy-zone").append(`<img src="${gif.images.original.url}">`);
-        }) 
+            $("#giphy-zone").append(`<img class="gif" src="${gif.images.original_still.url}" data-still="${gif.images.original_still.url}" data-animate="${gif.images.original.url}" data-state="still"><h3>Rating: ${gif.rating}</h3>`);
+        })
+        
+        $('.gif').on('click', function() {
+            var animate = $(this.attributes[3]);
+            var still = $(this.attributes[2]);
+            var state = $(this).attr('data-state');
+            if (state === 'still') {
+                $(this).attr({
+                    src: $(animate).val().trim(),
+                    'data-state': 'animate'
+                });
+            }
+            // console.log($(animate).val());
+            if (state === 'animate') {
+                $(this).attr({
+                  src: $(still).val().trim(),
+                  'data-state': 'still'
+                });
+            }
+
+        });
+        
     }
     );
-    
-});
-// 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+}
 
-// 5. Under every gif, display its rating (PG, G, so on).
-//    * This data is provided by the GIPHY API.
-//    * Only once you get images displaying with button presses should you move on to the next step.
+$(".artist-button").on("click", addGifs());
+
 
 // 6. Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
 
-// 7. Deploy your assignment to Github Pages.
-
-// 8. **Rejoice**! You just made something really cool.
-
-// - - -
-
-// ### Minimum Requirements
-
-// Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed. Adding a README.md as well as adding this homework to your portfolio are required as well and more information can be found below.
-
-// - - -
-
-// ### Bonus Goals
-
-// 1. Ensure your app is fully mobile responsive.
-
-// 2. Allow users to request additional gifs to be added to the page.
-//    * Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
-
-// 3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
-
-// 4. Integrate this search with additional APIs such as OMDB, or Bands in Town. Be creative and build something you are proud to showcase in your portfolio
-
-// 5. Allow users to add their favorite gifs to a `favorites` section.
-//    * This should persist even when they select or add a new topic.
-//    * If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies).
-
-// ### Reminder: Submission on BCS
-
-// * Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
-
-// - - -
-
-// ### Create a README.md
-
-// Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
-
-// * [About READMEs](https://help.github.com/articles/about-readmes/)
-
-// * [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
-
-// - - -
-
-// ### Add To Your Portfolio
-
-// After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
-
-// // - - -
